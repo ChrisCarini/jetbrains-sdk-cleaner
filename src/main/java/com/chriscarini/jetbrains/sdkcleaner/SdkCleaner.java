43dd1b17e48ad004acde1281c5363133c48453ac
Chris Carini <6374067+ChrisCarini@users.noreply.github.com>
2022-12-28 04:59:27 -0800

    Upgrading IntelliJ from 2022.2.4 to 2022.3.0 (#92)

--- src/main/java/com/chriscarini/jetbrains/sdkcleaner/SdkCleaner.java
+++ src/main/java/com/chriscarini/jetbrains/sdkcleaner/SdkCleaner.java
@@ -1,18 +1,20 @@
 package com.chriscarini.jetbrains.sdkcleaner;
 
-import com.chriscarini.jetbrains.sdkcleaner.messages.PluginMessages;
 import com.chriscarini.jetbrains.sdkcleaner.settings.SettingsManager;
 import com.intellij.ide.AppLifecycleListener;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.application.PreloadingActivity;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.projectRoots.Sdk;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-import static com.intellij.openapi.application.PathManager.*;
+import static com.intellij.openapi.application.PathManager.PROPERTY_PATHS_SELECTOR;
+import static com.intellij.openapi.application.PathManager.PROPERTY_SYSTEM_PATH;
 
 
 public class SdkCleaner extends PreloadingActivity implements AppLifecycleListener {
@@ -42,24 +44,23 @@ public class SdkCleaner extends PreloadingActivity implements AppLifecycleListen
         SdkUtils.printSDKs();
     }
 
+    @Nullable
     @Override
-    public void preload(@NotNull ProgressIndicator indicator) {
+    public Object execute(@NotNull Continuation<? super Unit> $completion) {
         // triggered. #2
         if (!SettingsManager.getInstance().getState().cleanOnStartup) {
-            return;
+            return null;
         }
 
         final List<Sdk> sdks = SdkUtils.getSDKs();
-        indicator.setText(PluginMessages.get("sdk.cleaner.opening.progress.indicator.in.progress", sdks.size()));
 
         LOG.debug("Before SDK cleaning...");
         SdkUtils.printSDKs();
 
-        SdkUtils.cleanSDKs(sdks, index -> indicator.setFraction((float) index.get() / sdks.size()), null);
+        SdkUtils.cleanSDKs(sdks, null);
 
         LOG.debug("After SDK cleaning...");
         SdkUtils.printSDKs();
-
-        indicator.setText(PluginMessages.get("sdk.cleaner.opening.progress.indicator.done", sdks.size()));
+        return null;
     }
 }
